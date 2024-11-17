2d62309e306bf3e0a7d165da107564f358ec394c
hemadri-git <bathulahemadri@gmail.com>
2024-11-17 22:01:12 +0530

    push

---
+++ src/com/patterns/bendSquare.java
@@ -0,0 +1,17 @@
+package com.patterns;
+
+public class bendSquare
+{
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<n-i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0; k<5; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/com/patterns/squareInsideSpacesRxC.java
@@ -0,0 +1,21 @@
+package com.patterns;
+
+public class squareInsideSpacesRxC
+{
+    public static void main(String[] args) {
+        int r = 4;
+        int c = 15;
+
+        for(int i = 0; i<r; i++){
+            for(int j =0; j<c; j++){
+                if (i == 0 || i == r-1 || j == 0 || j == c-1){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
