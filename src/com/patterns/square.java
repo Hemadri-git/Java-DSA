8a94ba0d33304a38b7affdd39789c86d98cd7c21
hemadri-git <bathulahemadri@gmail.com>
2024-11-17 19:13:33 +0530

    push

--- src/com/patterns/square.java
+++ src/com/patterns/square.java
@@ -4,6 +4,7 @@ public class square
 {
     public static void main(String[] args) {
 
+        System.out.println("pattern1");
         for(int i = 0; i<5; i++){
             for(int j = 0; j<5; j++){
                 System.out.print("*");
