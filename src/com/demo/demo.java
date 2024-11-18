617816f15ba96edd113c8281e5fd297b036e1703
hemadri-git <bathulahemadri@gmail.com>
2024-11-18 22:06:48 +0530

    push

--- src/com/demo/demo.java
+++ src/com/demo/demo.java
@@ -1,17 +1,26 @@
 package com.demo;
-import java.sql.SQLOutput;
-import  java.util.Scanner;
+
 public class demo
 {
     public static void main(String[] args) {
 
-        int length = 5;
 
-        for (int i = 0; i < length; i++){
-            for (int j =0; j <length; j++){
+        int r = 6;
+        int c = 6;
+
+        for (int i = 0; i<r; i++){
+            for (int j = 0; j<c-i; j++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j<c; j++){
                 System.out.print("*");
             }
+            for (int j = 0; j<c+i; j++){
+                System.out.print("*");
+            }
+
             System.out.println();
         }
+
     }
 }
