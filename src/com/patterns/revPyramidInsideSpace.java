deca6407a6bd42c99164f9d34b20fdad21e164a0
hemadri-git <bathulahemadri@gmail.com>
2024-11-20 20:04:46 +0530

    push

---
+++ src/com/patterns/revPyramidInsideSpace.java
@@ -0,0 +1,23 @@
+package com.patterns;
+
+public class revPyramidInsideSpace
+{
+    public static void main(String[] args) {
+        int n = 7;
+
+        for (int i = n-1; i>=0; i--){
+            for (int j = 0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0; k<2*i+1; k++){
+                if (k == 0 || k == 2*i || i == n-1){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
