2f673bb89f8f13d85dfd4691f135605210f6206c
hemadri-git <bathulahemadri@gmail.com>
2024-11-22 19:55:50 +0530

    push

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -4,20 +4,6 @@ public class Test
 {
     public static void main(String[] args) {
 
-        int n = 6;
-
-       for (int i = 0; i<n; i++){
-           for (int j = 0; j<2*(n-i-1); j++){
-               System.out.print(" ");
-           }
-           for (int k = i+1; k>=1; k--){
-               System.out.print(k+" ");
-           }
-           for(int l = 2; l<=i+1; l++){
-               System.out.print(l + " ");
-           }
-           System.out.println();
-       }
 
 
 
---
+++ src/com/demo/shop.java
@@ -0,0 +1,7 @@
+package com.demo;
+
+public interface shop
+{
+    void coffee();
+    void tea();
+}
