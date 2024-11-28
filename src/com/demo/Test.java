04a5de7681d5848560e358bc16be9f828cf54da6
hemadri-git <bathulahemadri@gmail.com>
2024-11-28 15:23:03 +0530

    push

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -1,14 +1,17 @@
 package com.demo;
 
-public class Test
-{
+public class Test {
     public static void main(String[] args) {
+        int arr[] = {7, 1, 5, 3, 10};
+        int n = arr.length;
+        int loss = arr[0];
+        int profit = 0;
 
-
-
-
-
-
-
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i] - loss;
+            profit = Math.max(temp, profit);
+            loss = Math.min(arr[i], loss);
+        }
+        System.out.println(profit);
     }
 }
