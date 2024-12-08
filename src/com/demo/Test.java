7047e5743d44b7233c0f430126ee086feac677e8
hemadri-git <bathulahemadri@gmail.com>
2024-12-08 21:28:54 +0530

    push

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -1,18 +1,17 @@
 package com.demo;
+import java.util.*;
+class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num[] = new int[n];
 
-public class Test {
-    static int age = 0;
-    static
-    {
-        age = 8;
-        System.out.println(age);
-    }
-    public static void main(String[] args)
-    {
+        for(int i = 0; i<n; i++){
+            num[i] = sc.nextInt();
+        }
 
-
-
-
-
+        for(int i = 0; i<n; i++){
+            System.out.print(num[i]);
+        }
     }
 }
