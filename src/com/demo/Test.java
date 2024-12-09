5a975bdf2c44aee738334ca3d1be071209009980
hemadri-git <bathulahemadri@gmail.com>
2024-12-09 22:16:07 +0530

    push

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -1,32 +1,14 @@
 package com.demo;
 
+import java.io.FileReader;
 
 class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        int[] num = {1,22,3,21,3,42,2};
-        int n = num.length;
-
-        for (int i =0; i<n-1; i++){
-            boolean swap = false;
-            for (int j = 0; j< n-i-1; j++){
-                if (num[j] > num[j+1]){
-                    int temp = num[j];
-                    num[j] = num[j+1];
-                    num[j+1] =temp;
-                    swap = true;
-                }
-            }
-            if (!swap){
-                break;
-
-            }
-        }
-
-        for (int i =0; i<n; i++){
-            System.out.print(num[i]+ " ");
-        }
+        FileReader file = new FileReader("array.java");
 
     }
+
+
 }
