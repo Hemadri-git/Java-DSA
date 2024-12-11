8a356351767e2608707c33328c6f891829456619
hemadri-git <bathulahemadri@gmail.com>
2024-12-11 21:20:44 +0530

    push

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -1,28 +1,10 @@
 package com.demo;
 
-class A extends Thread{
-   public void run(){
-       String name = Thread.currentThread().getName();
-       System.out.println(name);
-   }
-}
-class B extends Thread{
-    public void run(){
-        String name = Thread.currentThread().getName();
-        System.out.println(name);
-    }
-}
 public class Test
 {
     public static void main(String[] args) throws Exception
     {
 
-
-        A a = new A();
-        B b = new B();
-
-        a.start();
-        b.start();
 
         String name = Thread.currentThread().getName();
         System.out.println(name);
