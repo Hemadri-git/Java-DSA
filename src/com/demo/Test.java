1050193088cad9b4ee1acd79b115f57b4f450c2f
hemadri-git <bathulahemadri@gmail.com>
2024-12-11 22:30:52 +0530

    push

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -1,14 +1,31 @@
 package com.demo;
-
-public class Test
+class Sleep extends Thread
 {
-    public static void main(String[] args) throws Exception
+    @Override
+    public void run()
     {
 
-
-        String name = Thread.currentThread().getName();
-        System.out.println(name);
-
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println("i value is :"+i);
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e)
+            {
+                System.err.println("Thread interrupted "+e);
+            }
+        }
     }
 
+}
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Sleep s = new Sleep();
+        s.start();
+
+    }
 }
