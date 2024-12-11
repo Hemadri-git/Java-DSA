39c775ac7538ae0a93815d6de2060dc5f1004428
hemadri-git <bathulahemadri@gmail.com>
2024-12-11 21:19:11 +0530

    push

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -1,17 +1,31 @@
 package com.demo;
 
+class A extends Thread{
+   public void run(){
+       String name = Thread.currentThread().getName();
+       System.out.println(name);
+   }
+}
+class B extends Thread{
+    public void run(){
+        String name = Thread.currentThread().getName();
+        System.out.println(name);
+    }
+}
 public class Test
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        try
-        {
 
-        }
-        catch(Exception e)  //Exception and Throwable both are allowed
-        {
-            e.printStackTrace();
-        }
+
+        A a = new A();
+        B b = new B();
+
+        a.start();
+        b.start();
+
+        String name = Thread.currentThread().getName();
+        System.out.println(name);
 
     }
 
