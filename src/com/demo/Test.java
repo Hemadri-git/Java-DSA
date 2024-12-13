bd57f923b165e976f1e9e35b1f25f62a2a33d491
hemadri-git <bathulahemadri@gmail.com>
2024-12-13 21:48:40 +0530

    push

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -1,13 +1,52 @@
 package com.demo;
 
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Thread starts sleeping...");
-        try {
-            Thread.sleep(6000, 500000); // Sleep for 2 seconds and 500,000 nanoseconds
-        } catch (InterruptedException e) {
-            System.out.println("Thread was interrupted");
-        }
-        System.out.println("Thread wakes up");
+class Customer implements Runnable
+{
+    private int availableSeat = 1;
+    private int wantedSeat;  //1
+
+    public Customer(int wantedSeat)
+    {
+        super();
+        this.wantedSeat = wantedSeat;
     }
+
+    @Override
+    public synchronized void run()
+    {
+        String name = null;
+
+        if(availableSeat >= wantedSeat)
+        {
+            name = Thread.currentThread().getName();
+            System.out.println(wantedSeat+" seat is reserved for "+name);
+            availableSeat = availableSeat - wantedSeat;
+        }
+        else
+        {
+            name = Thread.currentThread().getName();
+            System.err.println("Sorry!!"+name+" seat is not available");
+        }
+
+
+    }
+
+}
+
+public class Test {
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        Customer c1 = new Customer(1);
+
+        Thread t1 = new Thread(c1,"Scott");
+        Thread t2 = new Thread(c1,"Smith");
+
+        t1.start();
+
+
+        t2.start();
+
+    }
+
 }
